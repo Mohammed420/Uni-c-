00efdadfdfb188644fb3e4e9c99fa125b72e109a
Mohammed420 <58623893+Mohammed420@users.noreply.github.com>
2022-04-02 01:19:42 +0300

    Update mainHOS.cpp

--- mainHOS.cpp
+++ mainHOS.cpp
@@ -2,17 +2,22 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <ctype.h>
+#include <string>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+void viewPat();
+void addPat();
+void seId(); // search by id
 
 struct patinet{
 
-	int id;
+	string id;
 	string name;
 	int age;
-	bool gender;
+	string gender;
 	string illness;
+	vector<string> meds; // medcine prescriped
 	string details; 
 
 
@@ -21,75 +26,91 @@ struct patinet{
 int main(int argc, char** argv) {
 	
 	int ui;
-	
-	ofstream file; // create a file for patients	
-	file.open("patinets.txt"); // open the file now call file inside programm
-	
-	if(file.fail()){ 
-		cerr << "File failed to open" << endl; // massage will apear if file failed to open
-		exit(1);
-	}
-	
-	patinet p1;
-	
-	
+	bool run=1;
 	
 	// main program
-	while (1){
-		
-		
-		
-		cout << "0-Close program 1-to enter new patient"; //Print options to user
+	while (run){
+		cout << "0-Close program 1-to enter new patient 2-to view all patient files"<<"  "; //Print options to user
 		cin >> ui;
 		if(ui == 0)
-			break; // to hault
-		if(ui == 1){
-			cout << "Entrer patient id: "; // patient id
-			cin >> p1.id;
-			file << "id: " << p1.i1d << endl;
-			cin.ignore(256, '\n');
-
-			cout << "Entrer patient name: "; // patient name
-			getline(cin, p1.name);
-			file << "name: "<< p1.name << endl;
-			cin.ignore(256, '\n');
-
-			cout << "Entrer patient age: "; // patient age
-			cin >> p1.age;
-			file << "Age: "<< p1.age << endl;
-			
-			cout << "Entrer patient gender 1 for male 0 for female: "; // patient gender
-			cin >> p1.gender;
-			switch(p1.gender){
-				case 0: p1.gender = "female";
-				case 1: p1.gender = "male";
-			}
-			file << "Gender: " << p1.gender << endl;
-			cin.ignore(256, '\n');
-			
-			cout << "Entrer patient illness: "; // patient illness
-			getline(cin, p1.illness);
-			file << "Illness: "<< p1.illness << endl;
-			
-			cout << "Entrer more details about patient: "; // patient more detials
-			getline(cin, p1.details);
-			file << p1.details << endl << endl;
-			
-			
+			run = 0; // to hault
+		else if(ui == 1){
+			patinet p1;
+			addPat();
+			cout << p1.id;
+			//file << p1.id;	
 		}
-		
+		else if(ui == 2){
+			cout << "All the patients: " << endl;
+			viewPat();
+		}
 	}
-	file.close();
-	
-	
 	return 0;
 }
 
+void addPat(){
+			fstream file; // create a file for patients	
+			file.open("patinets.txt", ios_base::app); // open the file now call file inside programm
+	
+			if(file.fail()){ 
+			cerr << "File failed to open" << endl; // massage will apear if file failed to open
+			exit(1);
+			}
+	
+			
+			patinet p1;
+			cout << "Enter patient id: "; // patient id
+			cin >> p1.id;
+			file << "id: " << p1.id << endl;
+			cin.ignore(1, '\n');
 
+			cout << "Enter patient name: "; // patient name
+			getline(cin, p1.name);
+			file << "name: " << p1.name << endl;
+			cin.ignore(1, '\n');
 
+			cout << "Enter patient age: "; // patient age
+			cin >> p1.age;
+			file << "age: " << p1.age << endl;
+			
+			cout << "Enter patient gender: "; // patient gender
+			cin >> p1.gender;
+			file << "Gender: " << p1.gender << endl;
+			cin.ignore(1, '\n');
+			
+			cout << "Enter patient illness: "; // patient illness
+			getline(cin, p1.illness);
+			file << "illness: " << p1.illness << endl;
+			
+			cout << "Enter more details about patient: "; // patient more detials
+			getline(cin, p1.details);
+			file << p1.details << endl << string(25, '=') <<endl;
+			
+			file.close();
+}
 
+void viewPat(){
+	fstream file;
+	string redFile;
+	
+	file.open("patinets.txt", ios_base::in);
+	if(file.fail()){ 
+			cerr << "File failed to open" << endl; // massage will apear if file failed to open
+			exit(1);
+	}
+	
+	while(file.good()){
+	getline(file, redFile);
+	cout << redFile << endl;
+	}
 
-
+	file.close();
+}
+void seId(){
+	
+	
+	
+}
 
 
 
